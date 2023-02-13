145beb89814ca6c7ad6ba8cae02ee86ad8026eab
danisaayu <danisafebriyani88@gmail.com>
2023-02-13 18:13:13 +0700

    inisial project

---
+++ Assets/Script/tombol 1.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tombol : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() 
+    {
+   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+
+    }
+
+    void OnMouseUp() 
+    {
+    	Application.LoadLevel ("halaman 2"); 
+
+    }
+
+}
